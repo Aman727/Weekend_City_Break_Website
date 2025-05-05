a1595d479de446ef708d56be51466bf95e3040fc
aman727 <aman.ahmed86@icloud.com>
2025-05-05 16:33:20 +0100

    Add simulation of user registration. User's inputs in the registration form are saved in localStorage

---
+++ scripts/simulate-registration.js
@@ -0,0 +1,21 @@
+function register(event) {
+
+    event.preventDefault();
+
+    //Get the values that the user entered into the register form
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    //Generate a unique key to store the user in local storage
+    const userId = "user_" + Math.random().toString(36).substr(2, 9);
+
+    //Create a user object
+    const user = {email, password};
+
+    //Store user in localStorage
+    localStorage.setItem(userId, JSON.stringify(user));
+}
+
+
+//Add an input listener onto the register button. When clicked the register() function will be called
+document.getElementById("registrationForm").addEventListener("submit", register);
